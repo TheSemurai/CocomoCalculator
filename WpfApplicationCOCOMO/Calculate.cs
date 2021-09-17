ae8ea4208afa2694998cddd49a19855d01909224
TheSemurai <maks.fa.27@gmail.com>
2021-09-17 11:38:29 +0300

    Add project files.

---
+++ WpfApplicationCOCOMO/Calculate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WpfApplicationCOCOMO
+{
+    public static class Calculate
+    {
+        //Розрахунок показника "трудоємність"
+        public static double GetLabor(double[] _arrayType, double _quantity) =>
+            _arrayType[0] * (Math.Pow(_quantity, _arrayType[1]));
+
+        //Розрахунок показника "тривалість"
+        public static double GetTime(double[] _arrayType, double _quantity) => 
+            _arrayType[2] * (Math.Pow(GetLabor(_arrayType, _quantity), _arrayType[3]));
+
+        //Розрахунок показника "розробники"
+        public static double GetChel(double[] _arrayType, double _quantity) =>
+            GetLabor(_arrayType, _quantity) / GetTime(_arrayType, _quantity);
+    }
+}
---
+++ WpfApplicationCOCOMO/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApplicationCOCOMO
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void GetResult(object sender, RoutedEventArgs e)
+        {
+            double[] _typeOfCalculate;
+            double _resultLabor, _resultTime, _resultChel;
+
+            switch (CmBoxText.Text)
+            {
+                case "Розповсюджений":
+                    _typeOfCalculate = new double[] { 2.4, 1.05, 2.5, 0.38 };
+                    UserStatusNotification();
+
+                    //Трудоємність
+                    _resultLabor = Calculate.GetLabor(_typeOfCalculate, Double.Parse(CodeSize.Text));
+                    Labor.Text = _resultLabor.ToString();
+
+                    //Тривалість
+                    _resultTime = Calculate.GetTime(_typeOfCalculate, Double.Parse(CodeSize.Text));
+                    Time.Text = _resultTime.ToString();
+
+                    //Розробники
+                    _resultChel = Calculate.GetChel(_typeOfCalculate, Double.Parse(CodeSize.Text));
+                    Chel.Text = _resultChel.ToString();
+                    GoodStickerThird.Visibility = Visibility;
+                    break;
+
+                case "Напівнезалежний":
+                    _typeOfCalculate = new double[] { 3.0, 1.12, 2.5, 0.35 };
+                    UserStatusNotification();
+
+                    //Трудоємність
+                    _resultLabor = Calculate.GetLabor(_typeOfCalculate, Double.Parse(CodeSize.Text));
+                    Labor.Text = _resultLabor.ToString();
+
+                    //Тривалість
+                    _resultTime = Calculate.GetTime(_typeOfCalculate, Double.Parse(CodeSize.Text));
+                    Time.Text = _resultTime.ToString();
+
+                    //Розробники
+                    _resultChel = Calculate.GetChel(_typeOfCalculate, Double.Parse(CodeSize.Text));
+                    Chel.Text = _resultChel.ToString();
+                    GoodStickerSecond.Visibility = Visibility;
+                    break;
+
+                case "Вбудований":
+                    _typeOfCalculate = new double[] { 3.6, 1.20, 2.5, 0.32 };
+                    UserStatusNotification();
+
+                    //Трудоємність
+                    _resultLabor = Calculate.GetLabor(_typeOfCalculate, Double.Parse(CodeSize.Text));
+                    Labor.Text = _resultLabor.ToString();
+
+                    //Тривалість
+                    _resultTime = Calculate.GetTime(_typeOfCalculate, Double.Parse(CodeSize.Text));
+                    Time.Text = _resultTime.ToString();
+
+                    //Розробники
+                    _resultChel = Calculate.GetChel(_typeOfCalculate, Double.Parse(CodeSize.Text));
+                    Chel.Text = _resultChel.ToString();
+                    GoodSticker.Visibility = Visibility;
+                    break;
+
+                default:
+                    ErrorSticker.Visibility = Visibility;
+                    ErrorMessage.Visibility = Visibility;
+                    break;
+            }
+        }
+
+        //Повідомлення користувача про помилку або вдале виконання
+        public void UserStatusNotification()
+        {
+            GoodSticker.Visibility = Visibility.Hidden;
+            GoodStickerSecond.Visibility = Visibility.Hidden;
+            GoodStickerThird.Visibility = Visibility.Hidden;
+            ErrorSticker.Visibility = Visibility.Hidden;
+            ErrorMessage.Visibility = Visibility.Hidden;
+        }
+    }
+}
